ea4215d6087c93aa79d1f8469af1dec907d023e6
Vatsalya Singh <singhvatsalya16@gmail.com>
2025-09-28 15:37:35 +0530

    indentation fixed , fixed some duplications , text moved into js as innerHTML with div

--- src/js/script.js
+++ src/js/script.js
@@ -119,57 +119,57 @@ function innertexts() {
   });
 
   sub_textone.forEach(el => {
-    el.innerHTML = ` "Understands human language our AI goes beyond keywords it grasps intent, context, and emotions to deliver accurate, meaningful responses every time" <div class="flex justify-center min-w-20">
+    el.innerHTML = `Understands human language our AI goes beyond keywords it grasps intent, context, and emotions to deliver accurate, meaningful responses every time<div class="flex justify-center min-w-20">
             <img src="../assets/Mastercard.svg" />
         </div> `
   });
 
   main_headertwo.forEach(el => {
-    el.innerText = "Natural Language Generation"
+    el.innerText = `Natural Language Generation`
   });
 
   sub_textthree.forEach(el => {
-    el.innerHTML = ` "Creates human like content from summaries and reports to stories and emails, our AI generates clear, fluent, and creative text tailored to your needs" <div class="flex justify-center min-w-20">
+    el.innerHTML = `Creates human like content from summaries and reports to stories and emails, our AI generates clear, fluent, and creative text tailored to your needs<div class="flex justify-center min-w-20">
             <img src="../assets/Mastercard.svg" />
         </div>`
   });
 
   speechai.forEach(el => {
-    el.innerText = "Speech AI"
+    el.innerText = `Speech AI`
   });
 
   speechsubs.forEach(el => {
-    el.innerHTML = ` "Listen and Speak naturally with advanced speech recognition and lifelike text-to-speech, our AI can hear you, understand you, and talk back seamlessly" <div class="flex justify-center min-w-20">
+    el.innerHTML = `Listen and Speak naturally with advanced speech recognition and lifelike text-to-speech, our AI can hear you, understand you, and talk back seamlessly<div class="flex justify-center min-w-20">
             <img src="../assets/Mastercard.svg" />
         </div> `
   });
 
   compvison.forEach(el => {
-    el.innerText = "Computer Vision"
+    el.innerText = `Computer Vision`
   });
 
   Reasoning.forEach(el => {
-    el.innerText = "Reasoning & Knowledge"
+    el.innerText = `Reasoning & Knowledge`
   });
 
   computerVision.forEach(el => {
-    el.innerHTML = ` "Sees and Understands Images upload photos or documents our AI detects objects, reads text, and even generates new images with stunning accuracy" <div class="flex justify-center min-w-20">
+    el.innerHTML = `Sees and Understands Images upload photos or documents our AI detects objects, reads text, and even generates new images with stunning accuracy<div class="flex justify-center min-w-20">
             <img src="../assets/Mastercard.svg" />
         </div>`
   });
 
   Reasoningsub.forEach(el => {
-    el.innerHTML = ` "Thinks and solves problems beyond conversation, our AI can analyze, plan, and connect data turning complex questions into smart, actionable answers" <div class="flex justify-center min-w-20">
+    el.innerHTML = `Thinks and solves problems beyond conversation, our AI can analyze, plan, and connect data turning complex questions into smart, actionable answers<div class="flex justify-center min-w-20">
             <img src="../assets/Mastercard.svg" />
-        </div>  `
+        </div>`
   });
 
   multimodel.forEach(el => {
-    el.innerText = "Multimodal Intelligence"
+    el.innerText = `Multimodal Intelligence`
   });
 
   multimodelsub.forEach(el => {
-    el.innerHTML = ` "Understands across Text, Image & Voice true next-gen AI that blends language, visuals, and audio. Ask a question, show an image, or speak it responds intelligently across all formats " <div class="flex justify-center min-w-20">
+    el.innerHTML = `Understands across Text, Image & Voice true next-gen AI that blends language, visuals, and audio. Ask a question, show an image, or speak it responds intelligently across all formats<div class="flex justify-center min-w-20">
             <img src="../assets/Mastercard.svg" />
         </div>`
   });
--- task.md
+++ task.md
@@ -1,2 +1,2 @@
 tasks --
-fix the dublicates ->  ###DONE ###
+fix the dublicates ->  ###DONE###
